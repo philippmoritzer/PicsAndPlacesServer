c318c9fd971f0e4ebc2e4c40b84b12f3329bc2cb
Philipp Moritzer <philipp.moritzer@yahoo.de>
2020-06-14 15:50:18 +0200

    refactored into models

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -14,7 +14,7 @@ exports.get_locations = async (req, res) => {
 
     await getLocations(req.query.category).then(result => {
         let locations = [];
-        result.forEach((item, index, array) => {
+        result.forEach((item) => {
             const location_item = location(item);
             locations.push(location_item);
         });
@@ -28,37 +28,7 @@ exports.get_locations = async (req, res) => {
 exports.get_location_by_id = async (req, res) => {
     await getLocationById(req.params.locationId).then(result => {
         result = result[0];
-        var location_item = {
-            "id": result.id,
-            "name": result.name,
-            "description": result.description,
-            "latitude": result.latitude,
-            "longitude": result.longitude,
-            "category": {
-                "id": result.categoryid,
-                "name": result.categoryname,
-                "hexcolor": result.hexcolor
-            },
-            "address": {
-                "country": {
-                    "name": result.countryname
-                },
-                "city": {
-                    "zipcode": result.zipcode,
-                    "name": result.cityname
-                },
-                "street": result.street,
-                "number": result.number
-            },
-            "mediaList": result.mediaFiles,
-            "createUser": {
-                "id": result.userid,
-                "name": result.username
-            },
-            "createdTime": result.created_time,
-            "updateTime": result.update_time
-        };
-
+        var location_item = location(result);
         res.status(200).json(location_item);
     }).catch(error => {
         res.status(200).send(error);
@@ -70,38 +40,8 @@ exports.get_location_by_name = async (req, res) => {
     console.log(req.params);
     await getLocationByName(req.params.locationName).then(result => {
         let locations = [];
-        result.forEach(async (item) => {
-            var location_item = {
-                "id": item.id,
-                "name": item.name,
-                "description": item.description,
-                "latitude": item.latitude,
-                "longitude": item.longitude,
-                "category": {
-                    "id": item.categoryid,
-                    "name": item.categoryname,
-                    "hexcolor": item.hexcolor
-                },
-                "address": {
-                    "country": {
-                        "name": item.countryname
-                    },
-                    "city": {
-                        "zipcode": item.zipcode,
-                        "name": item.cityname
-                    },
-                    "street": item.street,
-                    "number": item.number
-                },
-                "mediaList": item.mediaFiles,
-                "createUser": {
-                    "id": item.userid,
-                    "name": item.username
-                },
-                "createdTime": item.created_time,
-                "updateTime": item.update_time
-            };
-            locations.push(location_item);
+        result.forEach(item => {
+            locations.push(location(item));
         });
 
         res.status(200).json(locations);
@@ -116,57 +56,38 @@ exports.get_location_by_user_id = async (req, res) => {
     await getLocationByUserId(req.params.userId).then(result => {
         let locations = [];
         result.forEach(async (item) => {
-            var location_item = {
-                "id": item.id,
-                "name": item.name,
-                "description": item.description,
-                "latitude": item.latitude,
-                "longitude": item.longitude,
-                "category": {
-                    "id": item.categoryid,
-                    "name": item.categoryname,
-                    "hexcolor": item.hexcolor
-                },
-                "address": {
-                    "country": {
-                        "name": item.countryname
-                    },
-                    "city": {
-                        "zipcode": item.zipcode,
-                        "name": item.cityname
-                    },
-                    "street": item.street,
-                    "number": item.number
-                },
-                "mediaList": item.mediaFiles,
-                "createUser": {
-                    "id": item.userid,
-                    "name": item.username
-                },
-                "createdTime": item.created_time,
-                "updateTime": item.update_time
-            };
-            locations.push(location_item);
+            locations.push(location(item));
         });
-
         res.status(200).json(locations);
     }).catch(error => {
-        res.status(200).send(error);
+        res.status(500).json(error);
     });
 
 };
 
 exports.insert_location = async (req, res) => {
-    await insertLocation(req.body).then(result => {
-        res.status(200).json(result);
+    await insertLocation(req.body).then(async result => {
+        await getLocationById(result.insertId).then(result => {
+            result = result[0];
+            res.status(200).json(location(result));
+        }).catch(error => {
+            res.status(500).json(error);
+        });
+
     }).catch(error => {
+        console.log(error);
         res.status(500).json(error);
     });
 };
 
 exports.update_location = async (req, res) => {
-    await updateLocation(req.body).then(result => {
-        res.status(200).json(result);
+    await updateLocation(req.body).then(async result => {
+        await getLocationById(req.params.locationId).then(result => {
+            result = result[0];
+            res.status(200).json(location(result));
+        }).catch(error => {
+            res.status(500).json(error);
+        });
     }).catch(error => {
         res.status(500).json(error);
     })
